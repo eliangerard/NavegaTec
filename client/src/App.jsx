2b3a2bc03218aa74bc0cc89ef6139ec1e6abb2cd
eliangerard <eliangerardgg@gmail.com>
2024-05-01 20:02:19 -0600

    Add events route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { SchoolMap } from '../../shared/components/SchoolMap';
+import { Events } from '../../shared/pages/Events';
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path='/' element={<SchoolMap/>} />
+				<Route path='/events' element={<Events/>} />
 			</Routes>
 		</BrowserRouter>
 	);
