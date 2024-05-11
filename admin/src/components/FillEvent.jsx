8c8df99455815f7cf044c3c14ee8050c5cee7ce5
eliangerard <eliangerardgg@gmail.com>
2024-05-10 19:11:43 -0600

    Add functionality

--- admin/src/components/FillEvent.jsx
+++ admin/src/components/FillEvent.jsx
@@ -1,6 +1,62 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useRef, useState } from "react"
 import { BackButton } from "../../../shared/ui/BackButton"
+import { es } from 'date-fns/locale';
+import { DayPicker } from "react-day-picker"
+import 'react-day-picker/dist/style.css';
+import buildings from "../../../shared/data/buildings";
+import { Icon } from "../../../shared/ui/icons/Icon";
+import { useNavigate } from "react-router-dom";
 
-export const FillEvent = ({setStep}) => {
+export const FillEvent = ({ setStep, newEvent, setNewEvent, setEvents }) => {
+
+    const navigate = useNavigate();
+
+    const imgInput = useRef(null)
+    const datePicker = useRef(null)
+    const wherePicker = useRef(null)
+    const [showDatePicker, setShowDatePicker] = useState(false);
+    const [showWherePicker, setShowWherePicker] = useState(false);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (datePicker.current && !datePicker.current.contains(event.target)) {
+                setShowDatePicker(false);
+            }
+            if (wherePicker.current && !wherePicker.current.contains(event.target)) {
+                setShowWherePicker(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    });
+
+    const handleWhere = (where) => {
+        setNewEvent({ ...newEvent, where });
+        setShowWherePicker(false);
+    }
+
+    const handleAdd = () => {
+        setEvents(events => [...events, newEvent])
+        fetch('http://localhost:3000/events', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newEvent)
+        }).then(res => res.json()).then(data => {
+            console.log(data);
+            if (typeof newEvent.where === 'number')
+                setStep(3);
+            else
+                navigate('/')
+        })
+    }
+
     return (
         <>
             <div className="absolute top-8 left-8">
@@ -8,24 +64,118 @@ export const FillEvent = ({setStep}) => {
             </div>
             <h1 className="text-5xl text-center font-display font-bold">Aviso</h1>
             <p>¿Qué mostrará tu anuncio?</p>
-            <div className="grid grid-cols-10 w-fit grow gap-6 py-4">
-                <div className="col-span-4 bg-yellow border-2 border-black rounded-lg nt-shadow">
+            <div className="grid grid-cols-11 w-full grow gap-4 py-4">
+                <div className={`col-span-3 w-[200px] ${newEvent.type === 'administrative' ? 'bg-purple' : newEvent.type === 'security' ? 'bg-red' : newEvent.type === 'papers' ? 'bg-green' : newEvent.type === 'warning' ? 'bg-yellow' : 'bg-white'} border-2 w-full border-black rounded-lg nt-shadow p-4 flex flex-col justify-between items-center`}>
+                    <div className="flex flex-col items-center">
+                        <h2 className="text-3xl font-display font-bold text-center mb-4 text-ellipsis">{newEvent.title}</h2>
+                        <p className="text-center">{newEvent.description}</p>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        {newEvent.where != '' && <Icon className='w-24 max-h-20' building={newEvent.where} />}
 
+                        <div className="h-6 mt-4">{newEvent.button.length > 0 && <a target="_blank" href={newEvent.link} className="bg-red font-serif rounded-full px-6 h-fit border-2 border-black nt-shadow w-fit">{newEvent.button}</a>}</div>
+                    </div>
                 </div>
-                <div className="col-span-5 flex flex-col">
-                    <input type="text" className="border-2 mb-3 border-black rounded-3xl nt-shadow px-2 h-10 focus:outline-0" placeholder="Título del aviso" />
-                    <textarea className="border-2 mb-3 resize-none border-black rounded-3xl nt-shadow p-2 grow" type="text" placeholder="Descripción" />
-                    <input type="text" className="border-2 mb-3 border-black rounded-3xl nt-shadow px-2 h-10 focus:outline-0" placeholder="Título del botón" />
-                    <input type="text" className="border-2 border-black rounded-3xl nt-shadow px-2 h-10 focus:outline-0" placeholder="Link" />
+                <div className="col-span-6 flex flex-col w-full">
+                    <input type="text" className="border-2 mb-3 border-black rounded-3xl nt-shadow px-4 h-10 focus:outline-0" placeholder="Título del aviso"
+                        onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
+                        value={newEvent.title}
+                    />
+                    <textarea className="border-2 mb-3 resize-none border-black rounded-3xl nt-shadow p-4 grow focus:outline-0" type="text" placeholder="Descripción"
+                        onChange={(e) => setNewEvent({ ...newEvent, description: e.target.value })}
+                        value={newEvent.description}
+                    />
+                    <input type="text" className="border-2 mb-3 border-black rounded-3xl nt-shadow px-4 h-10 focus:outline-0" placeholder="Título del botón"
+                        onChange={(e) => setNewEvent({ ...newEvent, button: e.target.value })}
+                        value={newEvent.button}
+                    />
+                    <input type="text" className="border-2 border-black rounded-3xl nt-shadow px-4 h-10 focus:outline-0" placeholder="Enlace al que lleva el botón"
+                        onChange={(e) => setNewEvent({ ...newEvent, link: e.target.value })}
+                        value={newEvent.link}
+                    />
                 </div>
-                <div className="flex flex-col justify-between items-center">
+                <div className="col-span-2 flex flex-col justify-between items-center w-full">
                     <p className="my-2">¿Dónde?</p>
-                    <button className="bg-purple h-16 w-16 rounded-3xl border-2 border-black border-dashed"></button>
+                    <div className="relative">
+                        {showWherePicker &&
+                            <div ref={wherePicker} className="flex w-96 items-start p-2 max-h-60 overflow-auto mr-4 flex-wrap absolute top-1/2 -translate-y-1/2 right-full bg-purple rounded-md border-2 border-black nt-shadow">
+                                <h2 className="font-display font-bold text-center w-full text-xl pb-2">¿Será fuera de un edificio?</h2>
+                                {
+                                    [...Array(6)].map((_, i) =>
+                                        <button key={i} onClick={() => handleWhere(i + 1)}>
+                                            <Icon building={i + 1} />
+                                        </button>
+                                    )
+                                }
+                                <h2 className="font-display font-bold text-center w-full text-xl pb-2">¿Dónde se mostrará?</h2>
+                                {buildings.map((building, i) => (
+                                    <button onClick={() => handleWhere(building)} key={i} className="m-2">
+                                        <Icon building={building} />
+                                    </button>
+                                ))}
+                            </div>
+                        }
+                        <button onClick={() => setShowWherePicker(show => !show)} className="flex items-center justify-center bg-purple h-16 w-16 rounded-2xl border-2 border-black border-dashed">
+                            {
+                                newEvent.where != '' && <Icon building={newEvent.where} />
+                            }
+                        </button>
+                    </div>
                     <p className="my-2">¿Cuándo?</p>
-                    <button className="bg-purple h-16 w-16 border-2 border-black"></button>
+                    <div className="relative">
+                        {showDatePicker &&
+                            <div ref={datePicker}>
+                                <DayPicker className="absolute top-1/2 -translate-y-1/2 right-full bg-white rounded-xl border-2 border-black nt-shadow p-2 select-none" locale={es}
+                                    mode="single"
+                                    onSelect={(date) => setNewEvent({ ...newEvent, when: date })}
+                                    selected={newEvent.when}
+                                    modifiersClassNames={{
+                                        selected: 'bg-yellow rounded-full !border-1 border-black nt-shadow h-8 w-8 m-1',
+                                        disabled: 'bg-red',
+                                    }}
+                                />
+                            </div>
+                        }
+                        <button className="bg-purple h-16 w-16 rounded-md border-2 border-black px-2 font-serif text-xl"
+                            onClick={() => setShowDatePicker(show => !show)}
+                        >
+                            <p className="leading-none text-center min-w-fit w-1/2">{
+                                newEvent.when && new Date(newEvent.when).getDate() < 10 ?
+                                    `0${new Date(newEvent.when).getDate()}` :
+                                    new Date(newEvent.when).getDate()
+                            }
+                            </p>
+                            <div className="flex justify-end">
+                                <p className="leading-none text-center min-w-fit w-1/2">{
+                                    newEvent.when && new Date(newEvent.when).getMonth() + 1 < 10 ?
+                                        `0${new Date(newEvent.when).getMonth() + 1}` :
+                                        new Date(newEvent.when).getMonth() + 1
+                                }
+                                </p>
+                            </div>
+                        </button>
+                    </div>
                     <p className="my-2">Imágen</p>
-                    <button className="bg-purple h-16 w-16 rounded-xl border-2 border-black">+</button>
-                    <button className="bg-red rounded-full px-4 h-fit w-full border-2 border-black nt-shadow mt-4 w-fit">Añadir</button>
+                    <button className="bg-purple h-16 w-16 rounded-xl border-2 border-black"
+                        onClick={() => imgInput.current.click()}
+                    >
+                        +
+                    </button>
+                    <input ref={imgInput} className="hidden" type="file"
+                        accept="image/*"
+                        multiple={false}
+                        onChange={(e) => {
+                            const file = e.target.files[0];
+                            const reader = new FileReader();
+                            reader.onload = (e) => {
+                                setNewEvent({ ...newEvent, img: e.target.result });
+                            }
+                            reader.readAsDataURL(file);
+                        }}
+                    />
+                    <button className="bg-red rounded-full px-4 h-fit border-2 border-black nt-shadow w-fit mt-4"
+                        onClick={handleAdd}
+                    >Añadir</button>
                 </div>
             </div>
 
--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,34 +1,76 @@
 import { useState } from "react"
 import { SchoolMap } from "../../../shared/components/SchoolMap"
-import { useNavigate } from "react-router-dom"
 import { InitializeEvent } from "../components/InitializeEvent";
-import { BackButton } from "../../../shared/ui/BackButton";
 import { FillEvent } from "../components/FillEvent";
+import { Draggable } from "pigeon-maps";
+import { Icon } from "../../../shared/ui/icons/Icon";
+import { useNavigate } from "react-router-dom";
 
-export const Add = () => {
+const newEventTemplate = {
+    anchor: [28.71025950442226, -106.1059886447052],
+    title: "",
+    description: "",
+    button: "",
+    link: "",
+    where: "",
+    when: new Date(),
+    img: "",
+    type: "administrative",
+}
+
+export const Add = ({events, setEvents}) => {
 
     const navigate = useNavigate();
-    const [step, setStep] = useState(3);
-    const [events, setEvents] = useState([...Array(5).fill(null)]);
 
+    const [step, setStep] = useState(1);
+
+    const [newEvent, setNewEvent] = useState(newEventTemplate);
 
     return (
         <>
-            <SchoolMap />
-            {step != 3 ? <div className={`fixed overflow-hidden top-0 left-0 w-full h-full bg-black/25 flex items-center justify-center z-20 duration-[200ms] transition-all`}>
-                <div className="fixed flex flex-col left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 h-fit min-w-fit w-3/5 max-w-screen-lg bg-white rounded-2xl border-4 border-black nt-shadow items-center justify-center p-12">
+            <SchoolMap events={events} setEvents={setEvents} disabled>
+                <Draggable
+                    className="origin-center"
+                    anchor={newEvent.anchor}
+                    onDragMove={(anchor) => setNewEvent(event => ({ ...event, anchor }))}
+                    color='blue'
+                    width={50}
+                >
+                    <div className="relative">
+                        <div className="absolute -translate-x-1/2 -translate-y-1/2">
+                            <Icon building={newEvent.where} />
+                        </div>
+                    </div>
+                </Draggable>
+            </SchoolMap>
+            {step != 3 ? <div onClick={() => navigate('/')} className={`fixed overflow-hidden top-0 left-0 w-full h-full bg-black/25 flex items-center justify-center z-20 duration-[200ms] transition-all`}>
+                <div className="fixed flex flex-col left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 h-fit min-w-fit w-9/12 max-w-screen-lg bg-white rounded-2xl border-4 border-black nt-shadow items-center justify-center p-12">
 
-                    {step == 1 && <InitializeEvent />}
-                    {step == 2 && <FillEvent setStep={setStep} />}
+                    {step == 1 &&
+                        <InitializeEvent
+                            setStep={setStep}
+                            setNewEvent={setNewEvent}
+                        />}
+                    {step == 2 &&
+                        <FillEvent
+                            setStep={setStep}
+                            newEvent={newEvent}
+                            setNewEvent={setNewEvent}
+                            setEvents={setEvents}
+                        />}
 
                 </div>
             </div > :
                 <div className="fixed right-0 top-0 h-full bg-white w-[600px] z-20 border-l-4 border-black p-4 text-center">
-                    <h1 className="font-serif text-4xl font-bold ">Editorial</h1>
-                    <p>Arrastra, activa o desactiva del mapa o elimina algún anuncio.</p>
+                    <div className="flex justify-between">
+                        <div className="w-28"></div>
+                        <h1 className="font-serif text-4xl font-bold ">Editorial</h1>
+                        <button className="w-28 font-serif bg-white rounded-full border-2 nt-shadow border-black">Guardar</button>
+                    </div>
+                    <p className="my-2">Arrastra, activa o desactiva del mapa o elimina algún anuncio.</p>
                     <div className="flex flex-wrap p-2">
                         {events.map((e, i) => (
-                            <div className="flex-1 h-[400px] flex flex-col m-2 my-4" key={i}>
+                            <div draggable className="h-[400px] w-[250px] flex flex-col m-2 my-4" key={i}>
                                 <div className="h-10 border-4 border-b-0 border-black rounded-t-3xl mx-4 flex justify-center items-center">
                                     <button className="rounded-full border-2 border-black h-8 w-32 flex items-center">
                                         <div className="rounded-full h-6 w-6 border-2 border-black bg-green ml-0.5 mr-3"></div>
@@ -36,8 +78,15 @@ export const Add = () => {
                                     </button>
                                     <button className="ml-4">B</button>
                                 </div>
-                                <div className="bg-purple grow rounded-lg border-4 border-black nt-shadow">
-
+                                <div className={`col-span-4 ${newEvent.type === 'administrative' ? 'bg-purple' : newEvent.type === 'security' ? 'bg-red' : newEvent.type === 'papers' ? 'bg-green' : newEvent.type === 'warning' ? 'bg-yellow' : 'bg-white'} border-4 grow max-w-1/2 border-black rounded-lg nt-shadow p-4 flex flex-col justify-between items-center`}>
+                                    <div className="flex flex-col items-center">
+                                        <h2 className="text-3xl font-bold font-display text-center mb-4">{newEvent.title}</h2>
+                                        <p className="">{newEvent.description}</p>
+                                    </div>
+                                    <div className="flex flex-col items-center">
+                                        {newEvent.where != '' && <Icon className='w-24 max-h-20' building={newEvent.where} />}
+                                        {newEvent.button.length > 0 && <a target="_blank" href={newEvent.link} className="bg-red rounded-full px-6 text-lg h-fit border-2 border-black nt-shadow w-fit mt-8 font-serif">{newEvent.button}</a>}
+                                    </div>
                                 </div>
                             </div>
                         ))}
