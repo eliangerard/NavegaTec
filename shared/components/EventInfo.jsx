ec9a6b65b23368502cee259a9fb1c04f3d364729
eliangerard <eliangerardgg@gmail.com>
2024-05-10 19:26:35 -0600

    Fix server url

--- shared/components/EventInfo.jsx
+++ shared/components/EventInfo.jsx
@@ -14,7 +14,7 @@ export const EventInfo = ({ show, id, setShow }) => {
     });
 
     useEffect(() => {
-        fetch(`http://localhost:3000/events/${id}`)
+        fetch(`${import.meta.env.VITE_SERVER_URL}/events/${id}`)
             .then(res => res.json())
             .then(data => {
                 setEvent(data)
