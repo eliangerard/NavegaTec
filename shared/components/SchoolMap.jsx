b3f36745f15e7acfa05524fc76c8507d2c25b757
eliangerard <eliangerardgg@gmail.com>
2024-05-10 19:12:10 -0600

    Add location, bounds and events fetching

--- shared/components/SchoolMap.jsx
+++ shared/components/SchoolMap.jsx
@@ -5,9 +5,10 @@ import buildings from '../data/buildings';
 import { Icon } from '../ui/icons/Icon';
 import { BuildingInfo } from '../components/BuildingInfo';
 import { Link } from 'react-router-dom';
+import { EventInfo } from './EventInfo';
 
 
-export const SchoolMap = () => {
+export const SchoolMap = ({ children, disabled, events, moving }) => {
 
     const mapBounds = {
         sw: [
@@ -20,9 +21,13 @@ export const SchoolMap = () => {
         ]
     };
     const [showPopup, setShowPopup] = useState(false);
+    const [showEvent, setShowEvent] = useState(false);
     const [selectedBuilding, setSelectedBuilding] = useState(null);
-
+    const [selectedEvent, setSelectedEvent] = useState(null);
     const [center, setCenter] = useState([28.7080764, -106.1064929])
+
+
+    const [location, setLocation] = useState(null)
     const [style, setStyle] = useState(0);
     const styles = [
         (x, y, z) => { return `https://b.basemaps.cartocdn.com/rastertiles/voyager_nolabels/${z}/${x}/${y}.png` },
@@ -38,74 +43,128 @@ export const SchoolMap = () => {
         setShowPopup((show) => !show);
     }
 
+    const handleLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const { latitude, longitude } = position.coords;
+
+                if (latitude < mapBounds.sw[0] || latitude > mapBounds.ne[0] || longitude < mapBounds.sw[1] || longitude > mapBounds.ne[1]) {
+                    console.error("Location out of bounds");
+                    return;
+                }
+
+                setCenter([latitude, longitude]);
+                setLocation([latitude, longitude]);
+                console.log(latitude, longitude);
+                setInterval(() => {
+                    handleLocation();
+                }, 5000);
+            }, (error) => {
+                console.error(error);
+            });
+        } else {
+            console.error("Geolocation is not supported by this browser.");
+        }
+    }
+
     return (
         <>
+            <EventInfo id={selectedEvent} show={showEvent} setShow={setShowEvent} />
             <BuildingInfo id={selectedBuilding} show={showPopup} setShow={setShowPopup} />
-            <Map
-                provider={styles[style]}
-                defaultCenter={center}
-                defaultZoom={17}
-                minZoom={17}
-                maxZoom={20}
-                zoomSnap={false}
-                onBoundsChanged={({ center, zoom, bounds, initial }) => {
-                    if (!initial) {
-                        console.log(bounds);
-                        if (bounds.sw[0] < mapBounds.sw[0]) {
-                            bounds.sw[0] = mapBounds.sw[0];
+            <div className="w-full h-full overflow-hidden">
+                <Map
+                    provider={styles[style]}
+                    defaultCenter={center}
+                    center={center}
+                    defaultZoom={17}
+                    minZoom={17}
+                    maxZoom={20}
+                    zoomSnap={false}
+                    limitBounds='edge'
+                    onBoundsChanged={({ center }) => {
+                        setCenter(() => {
+                            const newCenter = center;
+                            if (newCenter[0] < mapBounds.sw[0]) newCenter[0] = mapBounds.sw[0];
+                            if (newCenter[0] > mapBounds.ne[0]) newCenter[0] = mapBounds.ne[0];
+                            if (newCenter[1] < mapBounds.sw[1]) newCenter[1] = mapBounds.sw[1];
+                            if (newCenter[1] > mapBounds.ne[1]) newCenter[1] = mapBounds.ne[1];
+                            return newCenter;
                         }
-                        if (bounds.sw[1] < mapBounds.sw[1]) {
-                            bounds.sw[1] = mapBounds.sw[1];
-                        }
-                        if (bounds.ne[0] > mapBounds.ne[0]) {
-                            bounds.ne[0] = mapBounds.ne[0];
-                        }
-                        if (bounds.ne[1] > mapBounds.ne[1]) {
-                            bounds.ne[1] = mapBounds.ne[1];
-                        }
+                        )
+                    }}
+                >
+                    {children}
+                    {
+                        buildings.map((building, i) => (
+                            <Marker
+                                style={{ pointerEvents: disabled ? 'none' : 'auto', filter: disabled ? 'grayscale(1)' : '' }}
+                                key={i}
+                                width={50}
+                                // longitude={building.lng}
+                                // latitude={building.lat}
+                                anchor={[building.lat, building.lng]}
+                                onClick={() => {
+                                    if (disabled) return;
+                                    handlePopup(building.id)
+                                }}
+                            >
+                                <div className="relative">
+                                    <div className="absolute translate-y-1/2 min-w-12">
+                                        <Icon building={building}
+                                        // style={{ transform: `scale(${zoom <= 17 ? (1 * Math.pow((zoom / 17), 6)) : 1})` }}
+                                        />
+                                    </div>
+                                </div>
+                            </Marker>
+                        ))
                     }
-                }}
-            >
-                {
-                    buildings.map((building, i) => (
-                        <Marker
-                            style={{ pointerEvents: 'auto' }}
-                            id={building.id}
-                            key={i}
-                            width={50}
-                            // longitude={building.lng}
-                            // latitude={building.lat}
-                            anchor={[building.lat, building.lng]}
-                            onClick={() => handlePopup(building.id)}
+                    {
+                        events.filter(event => typeof event.where === 'number').map((event, i) => (
+                            <Marker
+                                style={{ pointerEvents: disabled ? 'none' : 'auto', filter: disabled ? 'grayscale(1)' : '' }}
+                                key={i}
+                                width={50}
+                                anchor={event.anchor}
+                                onClick={() => {
+                                    setSelectedEvent(event._id);
+                                    setShowEvent(true);
+                                }}
+                            >
+                                <div className="relative">
+                                    <div className="absolute translate-y-1/2 min-w-12">
+                                        <Icon building={event.where} />
+                                    </div>
+                                </div>
+                            </Marker>
+                        ))
+                    }
+                    <Marker
+                        anchor={mapBounds.sw}
+                        color='red'
+                    />
+                    {
+                        location && <Marker
+                            anchor={location}
+                            color='blue'
                         >
-                            <Icon building={building}
-
-                            // style={{ transform: `scale(${zoom <= 17 ? (1 * Math.pow((zoom / 17), 6)) : 1})` }}
-                            />
+                            <div className='bg-blue-400 border-2 border-black nt-shadow rounded-full h-4 w-4'></div>
                         </Marker>
-                    ))
-                }
-                <Marker
-                    anchor={mapBounds.sw}
-                    color='red'
-                />
-                <Marker
-                    anchor={mapBounds.ne}
-                    color='green'
-                />
-                <Link to="/events" className='absolute right-0 w-12 m-4'>
-                    <img src="/annIcon.svg" alt="" />
-                </Link>
-                <ZoomControl />
-                <div className='absolute bottom-4 left-4 border-2 border-zinc-300 bg-white rounded-lg h-10 flex justify-around w-32 items-center'>
-                    <button onClick={() => setStyle((style - 1 + styles.length) % styles.length)} className='w-8'
-                    >{'<'}</button>
-                    <p className='w-8 text-center'>{style}</p>
-                    <button className='w-8'
-                        onClick={() => setStyle((style + 1) % styles.length)}
-                    >{'>'}</button>
-                </div>
-            </Map >
+                    }
+                    <Link to="/events" className='absolute right-0 w-12 m-4'>
+                        <img src="/annIcon.svg" alt="" />
+                    </Link>
+                    <ZoomControl />
+                    <div className='absolute bottom-4 left-4 border-2 border-zinc-300 bg-white rounded-lg h-10 flex justify-around w-32 items-center'>
+                        <button onClick={() => setStyle((style - 1 + styles.length) % styles.length)} className='w-8'
+                        >{'<'}</button>
+                        <p className='w-8 text-center'>{style}</p>
+                        <button className='w-8'
+                            onClick={() => setStyle((style + 1) % styles.length)}
+                        >{'>'}</button>
+                        <button onClick={handleLocation}>Ubi</button>
+                    </div>
+                </Map >
+            </div>
         </>
     )
 }
