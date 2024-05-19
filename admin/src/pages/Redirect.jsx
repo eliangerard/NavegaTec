323212937d1658fd045828b25044b224fa35145c
eliangerard <eliangerardgg@gmail.com>
2024-05-19 00:33:30 -0600

    Add code exchange

--- admin/src/pages/Redirect.jsx
+++ admin/src/pages/Redirect.jsx
@@ -1,14 +1,34 @@
-import { Navigate, useLocation, useParams, useSearchParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { UserContext } from "../context/UserProvider";
 
 export const Redirect = () => {
-    const { hash } = useLocation();
-    const params = new URLSearchParams(hash.substring(1));
-    const paramsJson = {};
-    for (const [key, value] of params.entries()) {
-        paramsJson[key] = value;
-    }
-    console.log(paramsJson);
-  return (
-    <Navigate to='/' />
-  )
+	const { search } = useLocation();
+	const params = new URLSearchParams(search);
+	const code = params.get("code");
+	const { setUser } = useContext(UserContext);
+
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		fetch(`${import.meta.env.VITE_SERVER_URL}/auth/token?code=${code}&redirectUri=${import.meta.env.VITE_REDIRECT_URI}`)
+			.then(res => res.json())
+			.then(data => {
+				localStorage.setItem("token", data.access_token);
+				localStorage.setItem("refresh", data.refresh_token);
+				setUser({ token: data.access_token, refresh: data.refresh_token, logged: true })
+				setLoading(false);
+			});
+	}, []);
+
+	return (
+		<>
+			{
+				loading ?
+					<h1>Loading...</h1>
+					:
+					<Navigate to="/" />
+			}
+		</>
+	);
 }
