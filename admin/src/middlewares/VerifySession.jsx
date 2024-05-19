f7fc7e0b346c4fe0ba3b37ef7fd87144f4fbada8
eliangerard <eliangerardgg@gmail.com>
2024-05-19 00:33:02 -0600

    Add verify session middleware

---
+++ admin/src/middlewares/VerifySession.jsx
@@ -0,0 +1,53 @@
+import { Route, Routes } from "react-router-dom"
+import { Login } from "../components/Login"
+import { Add } from "../pages/Add"
+import { Events } from "../../../shared/pages/Events"
+import { useContext, useEffect, useState } from "react"
+import { UserContext } from "../context/UserProvider"
+import { Home } from "../pages/Home"
+import { SquareLoader } from "react-spinners"
+import { SchoolMap } from "../../../shared/components/SchoolMap"
+
+export const VerifySession = () => {
+
+    const { user, setUser } = useContext(UserContext);
+    const [events, setEvents] = useState([]);
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_SERVER_URL}/events/`)
+            .then(res => res.json())
+            .then(data => setEvents(data))
+
+        if (localStorage.getItem("token") === null) {
+            setLoading(false);
+            return setUser({ logged: false });
+        }
+
+        fetch(`${import.meta.env.VITE_SERVER_URL}/auth/refresh`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("refresh")}`
+            }
+        }).then(res => res.json())
+            .then(data => {
+                setUser({ ...data, logged: true })
+                setLoading(false);
+            })
+    }, []);
+
+    return (
+        <Routes>
+            <Route path="/" element={loading ?
+                <>
+                    <SchoolMap display={false}/>
+                    <div className="fixed top-0 left-0 w-full h-full z-50 bg-black/25 flex items-center justify-center">
+                        <SquareLoader color="#000" size={50} loading={loading} />
+                    </div>
+                </>
+                : user.logged ? <Home /> : <Login />} />
+            <Route path="/add" element={<Add events={events} setEvents={setEvents} />} />
+            <Route path="/events" element={<Events events={events} />} />
+        </Routes>
+    )
+}
