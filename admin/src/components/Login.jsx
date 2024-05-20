d24028889725559077ab5dbd79c8b8b552e396a2
eliangerard <eliangerardgg@gmail.com>
2024-05-19 22:26:09 -0600

    Update style

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -12,23 +12,25 @@ export const Login = () => {
 
         <>
             {!user?.logged &&
-                <div className='fixed top-0 left-0 w-full h-full bg-black/25 z-20'>
-                    <div className='flex flex-col items-center border-4 nt-shadow p-12 w-1/3 min-h-fit min-w-fit rounded-lg bg-white border-black  absolute z-20 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2'>
-                        <h1 className='font-serif text-4xl font-bold text-center'>Inicia Sesión</h1>
-                        <div className='grow flex items-center'>
-                            {/* <MicrosoftLogin
-                        clientId={"a3a4079a-bf27-42d2-99c0-7a30e2739694"}
-                        authCallback={(error, response, msal) => console.log(error, response, msal)}
-                        tenantUrl='https://login.microsoftonline.com/4bc0e4bd-b054-4eb1-a4d3-ef13dc805095'
-                        redirectUri='http://localhost:5173/redirect/'
-                    /> */}
-                            <a
-                                className='flex border-zinc-400 border-2 p-3 text-center font-medium items-center mt-8 mb-4'
-                                href={`https://login.microsoftonline.com/4bc0e4bd-b054-4eb1-a4d3-ef13dc805095/oauth2/v2.0/authorize?client_id=a3a4079a-bf27-42d2-99c0-7a30e2739694&scope=a3a4079a-bf27-42d2-99c0-7a30e2739694/.default offline_access&redirect_uri=${import.meta.env.VITE_REDIRECT_URI}&response_type=code&response_mode=query`}
-                            >
-                                <img className="w-8" src={msLogo} alt="" />
-                                <p className="text-zinc-500 px-3">Ingresa con tu cuenta institucional</p>
-                            </a>
+                <div className='animate-fade fixed top-0 left-0 w-full h-full bg-black/25 z-20'>
+                    <div className='flex items-center overflow-hidden border-4 nt-shadow min-h-fit min-w-fit rounded-lg bg-white border-black  absolute z-20 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2'>
+                        <div className="relative flex items-center justify-center h-[400px] w-[450px]">
+                            <img className="absolute h-full w-full w-fit object-cover" src="/map-bg.png" alt="" />
+                            <img className="absolute p-6 z-20" src="/icon.png" alt="" />
+                        </div>
+                        <div className="p-12 w-full flex flex-col items-center">
+                            <div>
+                                <h1 className='font-serif text-5xl font-bold'>Inicia Sesión</h1>
+                                <div className='w-full items-center'>
+                                    <a
+                                        className='flex border-zinc-400 border-2 p-3 text-center font-medium items-center mt-8 mb-4'
+                                        href={`https://login.microsoftonline.com/4bc0e4bd-b054-4eb1-a4d3-ef13dc805095/oauth2/v2.0/authorize?client_id=a3a4079a-bf27-42d2-99c0-7a30e2739694&scope=a3a4079a-bf27-42d2-99c0-7a30e2739694/.default offline_access&redirect_uri=${import.meta.env.VITE_REDIRECT_URI}&response_type=code&response_mode=query`}
+                                    >
+                                        <img className="w-8" src={msLogo} alt="" />
+                                        <p className="text-zinc-500 text-sm px-3">Ingresa con tu correo institucional</p>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
