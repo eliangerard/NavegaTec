34ba4e3bf65e4438160ec6cac320dfa7d5cdbd00
eliangerard <eliangerardgg@gmail.com>
2024-05-19 22:27:43 -0600

    Fix style

--- admin/src/pages/Home.jsx
+++ admin/src/pages/Home.jsx
@@ -20,7 +20,6 @@ export const Home = () => {
             setAllEvents(data)
             setLoading(false);
         });
-        console.log("Jalando nueva data");
     }
 
     useEffect(() => {
@@ -34,9 +33,11 @@ export const Home = () => {
     }
 
     return (
-        <div className="flex h-full flex-1">
-            <SchoolMap events={events} />
-            <div className="relative flex flex-col right-0 top-0 h-full bg-white min-w-fit w-[60vh] z-20 border-l-4 border-black p-4 pb-0 text-center">
+        <div className="flex h-full">
+            <div className="flex-1 h-full">
+                <SchoolMap events={events} />
+            </div>
+            <div className="relative flex flex-col right-0 top-0 h-full bg-white z-20 w-[550px] border-l-4 border-black p-4 pb-0 text-center transition-all">
                 <div className="absolute bg-white border-4 border-black rounded-bl-2xl border-t-0 top-0 right-full p-2">
                     <h2 className="font-display font-bold">Acciones</h2>
                     <Link to={"/add"} className="flex flex-col items-center my-2">
@@ -55,13 +56,13 @@ export const Home = () => {
                     {/* <button className="w-28 font-serif bg-white rounded-full border-2 nt-shadow border-black">Guardar</button> */}
                 </div>
                 <p className="my-2 text-lg">Arrastra, activa, desactiva o elimina algún anuncio.</p>
-                <div className="overflow-y-auto grow">
+                <div className="overflow-y-scroll grow pb-2">
                     {loading ?
                         <div className="w-full h-full p-8 flex items-center justify-center">
                             <SquareLoader color="#000" size={50} loading={loading} />
                         </div>
                         :
-                        (allEvents.length > 0 ?
+                        (allEvents?.length > 0 ?
                             <div className="grid grid-cols-2 p-2 h-fit">
                                 {allEvents?.map((e) => (
                                     <PostedEvent key={`event${e._id}`} event={e} refresh={fetchEvents} />
