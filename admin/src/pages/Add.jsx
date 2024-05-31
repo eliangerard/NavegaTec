1155e49ea24c676820671031da8dcb7506722482
eliangerard <eliangerardgg@gmail.com>
2024-05-31 14:35:56 -0600

    Update styles

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -54,7 +54,7 @@ export const Add = ({ events, setEvents }) => {
                 <>
                     <SchoolMap events={events} setEvents={setEvents} display={false} />
                     <div className={`fixed overflow-hidden top-0 left-0 w-full h-full bg-black/25 flex items-center justify-center z-20 duration-[200ms] transition-all`}>
-                        <div ref={addOutside} className="fixed flex flex-col left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 h-fit min-w-fit w-9/12 max-w-screen-lg bg-white rounded-2xl border-4 border-black nt-shadow items-center justify-center p-12 z-30">
+                        <div ref={addOutside} className="fixed flex flex-col left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 h-fit min-w-fit w-9/12 w-full lg:max-w-screen-lg bg-white rounded-2xl border-4 border-black nt-shadow items-center justify-center p-12 z-30">
                             {step == 1 &&
                                 <InitializeEvent
                                     setStep={setStep}
