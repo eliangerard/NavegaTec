936d2b80e65ea0affd72d95f711a87dc0b1e7b7d
eliangerard <eliangerardgg@gmail.com>
2024-05-31 14:37:40 -0600

    Add popup logic and fix styles

--- shared/ui/Event.jsx
+++ shared/ui/Event.jsx
@@ -3,14 +3,15 @@ import { EventInfo } from "../components/EventInfo";
 import { Icon } from "./icons/Icon"
 import { useState } from 'react';
 
-export const Event = ({ _id, title, where, when, button, description, type, i, col }) => {
+export const Event = ({ _id, popup = false, title, where, when, button, description, type, i, col }) => {
     const [showInfo, setShowInfo] = useState(false);
+    const large = (col === 1 ? (i % 3 == 0 || i == 0) : ((i - 1) % 3 == 0));
 
     return (
         <>
-            {showInfo && <EventInfo show={showInfo} setShow={setShowInfo} id={_id} />}
+            {showInfo && <EventInfo popup={popup} show={showInfo} setShow={setShowInfo} id={_id} />}
             <div
-                className={`animate-fade flex flex-col justify-between ${(col === 1 ? (i % 3 == 0 || i == 0) : ((i - 1) % 3 == 0)) ? 'sm:col-span-2 h-[310px]' : 'col-span-1 h-[380px]'} ${type === 'administrative' ? 'bg-purple' : type === 'security' ? 'bg-red' : type === 'papers' ? 'bg-green' : 'bg-white'} p-4 rounded-lg shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] border-2 border-black`}
+                className={`animate-fade flex flex-col justify-between ${ large ? 'sm:col-span-2 h-[250px]' : 'col-span-1 h-[380px]'}  ${type === 'administrative' ? 'bg-purple' : type === 'security' ? 'bg-red' : type === 'papers' ? 'bg-green' : type === 'warning' ? 'bg-yellow' : 'bg-white'} p-4 rounded-lg shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] border-2 border-black`}
                 onClick={(e) => {
                     e.stopPropagation();
                     setShowInfo(true);
@@ -18,29 +19,29 @@ export const Event = ({ _id, title, where, when, button, description, type, i, c
             >
                 <div>
                     <div className="flex">
-                        {type === 'events' && <Icon building={where} />}
-                        {type === 'events' &&
+                        {large && <Icon className="h-12 my-0" building={where} />}
+                        {large &&
                             <div className='ml-2'>
-                                <h3 className='text-lg font-display font-semibold'>{title}</h3>
+                                <h3 className='text-xl font-display font-bold'>{title.toUpperCase()}</h3>
                                 {when && <p className='text-xs '>
                                     {`${new Date(when).toLocaleDateString('es-ES', { weekday: 'long' })}`}
                                 </p>}
                             </div>
                         }
                     </div>
-                    {type !== 'events' && <h3 className='text-3xl font-display font-bold text-center text-ellipsis w-full truncate h-8'>{title}</h3>}
-                    <p className={`${(col === 1 ? (i % 3 == 0 || i == 0) : ((i - 1) % 3 == 0)) ? 'max-h-[75px] text-left line-clamp-3' : 'h-[140px] text-center line-clamp-6'} w-full my-2 text-pretty hyphens-auto`}>
+                    {!large && <h3 className='text-3xl font-display font-bold text-center text-ellipsis w-full truncate h-8'>{title.toUpperCase()}</h3>}
+                    <p className={`${large ? 'max-h-[75px] text-left line-clamp-3' : 'h-[140px] text-center line-clamp-6'} w-full my-2 text-pretty hyphens-auto`}>
                         {description}
                     </p>
                 </div>
-                {type !== 'events' &&
+                {!large &&
                     <div className="w-full flex justify-center">
-                        <Icon building={where} className='h-20 my-2' />
+                        <Icon building={where} className='h-20 my-2' fontSize="text-3xl"/>
                     </div>
                 }
                 <div className='flex justify-between w-full items-end'>
-                    {button && <button className='bg-red border-2 rounded-full h-8 border-black px-4 shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] max-w-32 font-serif'>
-                        <p className="text-truncate w-full h-full">
+                    {button && <button className='bg-red border-2 rounded-full h-8 border-black px-4 shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] w-fit font-serif'>
+                        <p className="w-fit">
                             {button}
                         </p>
                     </button>}
