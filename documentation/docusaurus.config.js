0954a1e7f441ed661cc2d4dbbc976c6c476b7123
eliangerard <eliangerardgg@gmail.com>
2024-06-04 21:46:41 -0600

    Update documentation configuration and URLs

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -9,14 +9,14 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'NavegaTec',
-  tagline: 'Documentación del Frontend',
+  tagline: 'Documentación del proyecto',
   favicon: 'img/icon.png',
 
   // Set the production url of your site here
   url: 'https://navegatest.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/navegatec/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
